155e74d30dfe8f1332e4ff6393e3fd597d79abe8
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-09-23 00:22:34 +0000

    used template literals to refine code

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🌈 Assertion Passed " + actual + " === " + expected);
+    console.log(`🌈 Assertion Passed ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log("🌋 Assertion Failed " + actual + " !== " + expected);
+    console.log(`🌋 Assertion Failed ${actual} !== ${expected}`);
   }
 };
 
