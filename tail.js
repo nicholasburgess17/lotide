e2086aa5b87d71ec0b5bf9fc973960351f9f3bc9
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-09-23 03:03:50 +0000

    created a new array within the tail function

--- tail.js
+++ tail.js
@@ -8,8 +8,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  return array.slice[1];
+  let newArr = array.slice(1);
+  return newArr;
 };
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail(words))
