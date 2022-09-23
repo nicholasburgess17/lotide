c6feaf072828fb96ede45f0c8067d566bd2621ed
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-09-23 00:11:08 +0000

    created an assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🌈 Assertion Passed " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("🌋 Assertion Failed " + actual + " !== " + expected);
+  }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("yes", "yes");
+assertEqual(7, 9);
