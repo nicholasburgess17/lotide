b628ebb1461115978d5f4491416889921eeccb22
nicholasburgess17 <nicholas.burgess13@gmail.com>
2022-09-28 20:05:11 +0000

    2 functions added, countOnly and countLetters

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🌈 Assertion Passed ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🌋 Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const result = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+        console.log(letter);
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+const string = "this should be shorter";
+const result2 = countLetters(string, " ");
