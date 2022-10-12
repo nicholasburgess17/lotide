f2f4147582b40dd71a38a387fc0aa81bd9cfc17f
nicholasburgess17 <nicholas.burgess13@gmail.com>
2022-10-12 21:48:51 +0000

    refactored head and assertEqual functions

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🌋 Assertion Failed ${actual} !== ${expected}`);
   }
 };
+module.exports = assertEqual;
 
-//test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("yes", "yes");
-assertEqual(7, 9);2
--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🌈 Assertion Passed ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`🌋 Assertion Failed ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
+
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 3);
-assertEqual(head([77]), 77);
+
+module.exports = head
