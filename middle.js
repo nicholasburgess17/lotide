89e9bd528ae4352a2e1d2466c28b22c404dd8dc0
nicholasburgess17 <nicholas.burgess13@gmail.com>
2022-10-12 22:28:45 +0000

    refactored middle.js

--- middle.js
+++ middle.js
@@ -1,46 +1,3 @@
-const assertArraysEqual = function (arr, arrTwo) {
-  if (arr.length !== arrTwo.length) {
-    console.log(`🌋 Assertion Failed ${arr} !== ${arrTwo}`);
-    return false;
-  }
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] !== arrTwo[i]) {
-      console.log(`🌋 Assertion Failed ${arr} !== ${arrTwo}`);
-      return false;
-    }
-
-    console.log(`🌈 Assertion Passed ${arr} === ${arrTwo}`);
-    return true;
-  }
-}; 
-const eqArrays = function (arr, arrTwo) {
-  if (arr.length !== arrTwo.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] !== arrTwo[i]) {
-      return false;
-    }
-
-    return true;
-  }
-};
-
-/*const middle = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    // sorts into even and odds
-    if (array % 2 === 0) {
-      let even = [];
-    }
-    //sorts odds, (may not be necessary)
-    if (array % 2 !== 0) {
-      let odd = [];
-    }
-  }
-};
-*/
 const middle = function (array) {
   if (array.length <= 2) {
     return []
@@ -51,22 +8,4 @@ const middle = function (array) {
   
 };
 
-//TEST CASES
-
-console.log(middle([1, 2, 3, 4])) // [2, 3]
-console.log(middle([1, 2, 3])) // [2]
-console.log(middle([1])) // []
-console.log(middle([1, 2])) // []
-
-/*
-assertArraysEqual(middle([1, 2, 3, 4], [2, 3]))
-assertArraysEqual(middle([1, 2, 3], [2]))
-assertArraysEqual(middle([1, 2], []))
-assertArraysEqual(middle([1], []))
-*/
-/*
-eqArrays((middle([1, 2, 3, 4]), [2,3])); // [2, 3]
-eqArrays((middle([1, 2, 3]),[2])); // [2]
-eqArrays((middle([1]), [])); // []
-eqArrays((middle([1, 2]), [])); // []
-*/
+module.exports = middle
