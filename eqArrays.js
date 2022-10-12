7e55682d54254bc53d86f42b4798f85d69734b94
nicholasburgess17 <nicholas.burgess13@gmail.com>
2022-10-12 22:16:52 +0000

    refactored eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,47 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🌈 Assertion Passed ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`🌋 Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-
-/*const eqArrays = function (arr , arrTwo) {
-  let newArr = []
-  let newArrTwo = []
-  for (let i = 0; i < arr.length; i++) {
-   newArr = arr
-   for (let j = 0; j < arrTwo.length; j++){
-    newArrTwo = arrTwo
-   }
-    if (newArr === newArrTwo) {
-      return true
-  } else {
-    return false
-  }
-  } 
-  console.log(newArr)
-  console.log(arrTwo)
-} 
-
-*/ /*
-const eqArrays = function (arr , arrTwo) {
-  let nArr = [];
-  let nArrTwo = [];
-  nArr += arr
-  nArrTwo += arrTwo
-
-  console.log(nArrTwo)
-  console.log(nArr)
-  if (nArr === nArrTwo) {
-    return true
-  } else {
-    return false
-  }
-} 
-*/
-
 const eqArrays = function (arr, arrTwo) {
   if (arr.length !== arrTwo.length) {
     return false;
@@ -56,25 +12,4 @@ const eqArrays = function (arr, arrTwo) {
   }
 };
 
-/*
-const eqArrays = function (arr , arrTwo) {
-  if (arr === arrTwo) {
-    console.log(true)
-  } else {
-    console.log(false)
-  }
-}
-*/
-(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
-const equal = assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
-
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
-
-
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
-(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
-
-const result = eqArrays(["1", "2", "3"], ["1", "2", 3]);
-(result)
+module.exports = eqArrays
