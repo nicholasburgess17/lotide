0e3930f5d2d39ec606225d47a966cb14c9483d7d
nicholasburgess17 <nicholas.burgess13@gmail.com>
2022-10-13 00:20:13 +0000

    created an index file to ensure lotide is available with one import

---
+++ index.js
@@ -0,0 +1,40 @@
+//functions
+const head = require('./head')
+const tail = require('./tail')
+const middle = require('./middle')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const flatten = require('./flatten')
+const findKeyByValue = require('./findKeyByValue')
+const findKey = require('./findKey')
+const letterPositions = require ('./letterPositions')
+const map = require('./map')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+//assertions
+const assertArraysEqual = require('./assertArrayEqual.js')
+const assertEqual = require('./assertEqual.js')
+const eqArrays = require('./eqArrays.js')
+const eqObjects = require('./eqObjects')
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+  eqObjects: eqObjects,
+  countOnly: countOnly,
+  flatten: flatten,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+
+
+};
